580032f360e166f51358cae2e1707348ee91db9c
Meredith Davis <meredithd23@yahoo.com>
2020-10-05 20:58:13 -0500

    updates

--- src/Router.js
+++ src/Router.js
@@ -8,10 +8,23 @@ to the About component.
 Remember to export the component at the end
 */
 
-import React from 'react'
-import { Switch, Route } from 'react-router'
+import React from "react";
+import { Switch, Route } from "react-router";
+import Home from "./components/Home";
+import About from "./components/About";
+import Car from "./components/Car";
 // Write component imports here //
 
-
+const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/About" component={About} />
+      <Route path="/Car/:id" component={Car} />
+    </Switch>
+  );
+};
 
 // Start Router function here //
+
+export default Router;
